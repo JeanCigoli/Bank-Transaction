4e731a277c0fe2589edf4e2dd6d52820ad9a2e96
jeancigoli <jean@primi.com.br>
2020-12-02 21:18:16 -0300

    feat: created dto for statistic

---
+++ bank/src/main/java/com/transation/bank/entity/dto/Statistic.java
@@ -0,0 +1,48 @@
+package com.transation.bank.entity.dto;
+
+public class Statistic {
+
+	private int count;
+	private Double sum, avg, min, max;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+
+	public Double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(Double avg) {
+		this.avg = avg;
+	}
+
+	public Double getMin() {
+		return min;
+	}
+
+	public void setMin(Double min) {
+		this.min = min;
+	}
+
+	public Double getMax() {
+		return max;
+	}
+
+	public void setMax(Double max) {
+		this.max = max;
+	}
+
+}
